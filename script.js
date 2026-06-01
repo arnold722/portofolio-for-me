d2a6472f5fab87b3d937fcc9119c7ecc97cfbf2e
arnold722 <leoncenarnolde@gmail.com>
2026-06-01 22:11:52 +0200

    add a text

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ document.addEventListener('keydown', event => {
         hideSidebar();
     }
 });
+
+const carousel = document.getElementById("carousel");
+let rotY=0, drag=false, startX;
+const panels = carousel.querySelectorAll(".panel");
+document.addEventListener("mousedown", e=>{ drag=true; startX=e.clientX; });
+document.addEventListener("mouseup", ()=> drag=false );
+document.addEventListener("mousemove", e=>{
+    if(!drag) return;
+    rotY += (e.clientX-startX)*0.4;
+    carousel.style.transform=`rotateY(${rotY}deg)`;
+    startX = e.clientX;
+    updatePanels();
+});
+function auto() {
+    if(!drag){ rotY+=0.25; carousel.style.transform=`rotateY(${rotY}deg)`; updatePanels();}
+    requestAnimationFrame(auto);
+}
